d97ae6a80ee4bd5c62fdf3ff27e97dd6b838dd52
adarshkavallore <adarshsjv00@gmail.com>
2020-02-11 15:39:27 +0530

    Add files via upload

---
+++ tdd-demo-mummifier/Mummifier/Mummifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+
+namespace Mummifier
+{
+    public static class Mummifier
+    {
+        public static string Mummify(string input)
+        {
+            throw new NotImplementedException("Implement with TDD");
+        }
+
+    }
+}
